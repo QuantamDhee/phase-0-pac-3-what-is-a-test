f2c87d2c4d21a6ca3aba91d477e1f6da12043652
Johndhee <you@example.com>
2021-04-02 05:11:47 -0700

    done

--- test/index-test.js
+++ test/index-test.js
@@ -1,5 +1,13 @@
 const index = require("../index.js");
 
+const name = "Susan";
+const height = 39;
+const message = `${name} is ${height} inches tall`;
+
+module.exports = {name, height, message};
+
+console.log(message);
+
 /*
 describe('what-is-a-test', () => {
   describe('Name', () => {
